11d7a98dcd338f7a19335d31fb08ebc589c812f2
Kuziwa Sachikonye <sach@sach.co.za>
2018-10-30 15:50:35 +0200

    created smoke test - total supply set on contract creation

---
+++ test/SachToken.js
@@ -0,0 +1,13 @@
+var SachToken = artifacts.require("../contracts/SachToken.sol");
+
+contract('SachToken', function(accounts) {
+  it('sets the total supply upon deployment', function(){
+    return SachToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.totalSupply();
+    }).then(function(totalSupply) {
+      assert.equal(totalSupply.toNumber(), 1000001)
+    });
+
+  });
+})
