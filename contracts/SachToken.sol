2667e99e4e4327b5a758f5cd36f8aa6d0b702242
Kuziwa Sachikonye <sach@sach.co.za>
2018-10-30 17:32:22 +0200

    allocate initial supply to the admin account

--- contracts/SachToken.sol
+++ contracts/SachToken.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.4.23;
 contract SachToken {
   //N.B: solidity gives us a free getter function to retun the total supply.
   uint256 public totalSupply;
+
+  mapping (address => uint256) public balanceOf;
+
   //constructor
-  function SachToken(uint256 _initialSupply) public {
+  constructor (uint256 _initialSupply) public {
     totalSupply = _initialSupply;
+    //allocate the inital supply to admin account
+    balanceOf[msg.sender] = _initialSupply;
   }
 }
--- test/SachToken.js
+++ test/SachToken.js
@@ -1,13 +1,25 @@
 var SachToken = artifacts.require("../contracts/SachToken.sol");
 
 contract('SachToken', function(accounts) {
+
   it('sets the total supply upon deployment', function(){
     return SachToken.deployed().then(function(instance) {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply) {
-      assert.equal(totalSupply.toNumber(), 1000000)
+      assert.equal(totalSupply.toNumber(), 1000000, 'set the total supply to 1,000,000');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(adminBalance){
+      assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial balance to the admin account');
+    });
     });
 
+    it('allocates the inital supply to the admin account', function(){
+      return SachToken.deployed().then(function(instance) {
+        tokenInstance = instance;
+        return tokenInstance.balanceOf(accounts[0]);
+      }).then(function(adminBalance){
+        assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial balance to the admin account');
+      });
+      });
   });
-})
