eec637a3fd833d08d53efd14585eb9434b2fbb94
Kuziwa Sachikonye <sach@sach.co.za>
2018-11-01 10:16:24 +0200

    approve an account to spend an alocated amount of tokens in msg.sender's account

--- contracts/SachToken.sol
+++ contracts/SachToken.sol
@@ -10,13 +10,25 @@ contract SachToken {
   //N.B: solidity gives us a free getter function to retun the total supply.
   uint256 public totalSupply;
 
+  //transfer event
   event Transfer(
     address indexed _from,
     address indexed _to,
     uint256 _value
   );
+  //approve event
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+  //transfer
 
+  //balanceof : returns the amount of tokens for each address supplied. Keeps track of tokens and their owners.
   mapping (address => uint256) public balanceOf;
+
+  //allowance : returns the mapping for each of the addresses that a particular owner has approved. For each of these approved addresses, the amount approved. Keeps track of the amount of tokens approved and to whom.
+  mapping (address => mapping (address => uint256))public allowance;
 
   //constructor
   constructor (uint256 _initialSupply) public {
@@ -38,5 +50,16 @@ contract SachToken {
     //Return an Boolean
     return true;
   }
+
+  //approve a transfer  //balanceof : return the amount of tokens for each address supplied. Keeps track of tokens and their owners.
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    //allowance
+    allowance[msg.sender][_spender] = _value;
+    //Approve Event
+    emit Approval(msg.sender, _spender, _value);
+    //returnthe mapping for each of the addresses that a particular owner has approved. For each of these approved addresses, the amount approved. Keeps track of the amount of tokens approved and to whom. a boolean
+    return true;
+  }
+  //transfer from
 
 }
